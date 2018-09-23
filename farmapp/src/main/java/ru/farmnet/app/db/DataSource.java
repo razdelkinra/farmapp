51f63d31214bd4e8fb32657c8a162fc252b6dbda
Razdelkin Ruslan <rrazdelkin@gmail.com>
2018-09-24 00:13:15 +0300

    Absolute URL DB

--- farmapp/src/main/java/ru/farmnet/app/db/DataSource.java
+++ farmapp/src/main/java/ru/farmnet/app/db/DataSource.java
@@ -3,6 +3,7 @@ package ru.farmnet.app.db;
 import com.mchange.v2.c3p0.ComboPooledDataSource;
 
 import java.beans.PropertyVetoException;
+import java.io.File;
 import java.sql.Connection;
 import java.sql.SQLException;
 
@@ -11,7 +12,7 @@ public class DataSource {
     private static DataSource dataSource;
     private ComboPooledDataSource comboPooledDataSource;
     static final String JDBC_DRIVER = "org.firebirdsql.jdbc.FBDriver";
-    static final String DB_URL = "jdbc:firebirdsql:localhost/3050:C:/firebird/FAR.FDB";
+    static final String DB_URL = "jdbc:firebirdsql:localhost/3050:";
     static final String USER = "SYSDBA";
     static final String PASSWORD = "masterkey";
 
@@ -19,12 +20,18 @@ public class DataSource {
         try {
             comboPooledDataSource = new ComboPooledDataSource();
             comboPooledDataSource.setDriverClass(JDBC_DRIVER);
-            comboPooledDataSource.setJdbcUrl(DB_URL);
+            comboPooledDataSource.setJdbcUrl(getURL());
             comboPooledDataSource.setUser(USER);
             comboPooledDataSource.setPassword(PASSWORD);
         } catch (PropertyVetoException ex1) {
             ex1.printStackTrace();
         }
+    }
+
+    private String getURL() {
+        String abspath = new File("resources/bd/FAR.FDB").getAbsolutePath().replace("\\","/");
+        String path = new File("resources/bd/FAR.FDB").getPath().replace("\\","/");
+        return DB_URL + abspath.substring(0, abspath.indexOf(path)) + "farmapp/src/main/" + path;
     }
 
     public static DataSource getInstance() {
